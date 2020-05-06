e90e0dff1140e8c9736a006faad51632df7734c2
Daniel Abergel <daniel.abergel@ibm.com>
2020-05-06 14:35:40 +0300

    adding project files

---
+++ Assets/Scripts/1-movers/Mover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/**
+ * This component moves its object in a fixed velocity.
+ * NOTE: velocity is defined as speed+direction.
+ *       speed is a number; velocity is a vector.
+ */
+public class Mover: MonoBehaviour {
+    [Tooltip("Movement vector in meters per second")]
+    [SerializeField] Vector3 velocity;
+
+    void Update() {
+        transform.position += velocity * Time.deltaTime;
+        if (this.transform.position.y < -250)
+            Destroy(this.gameObject);
+    }
+
+    public void SetVelocity(Vector3 newVelocity) {
+        this.velocity = newVelocity;
+    }
+}
